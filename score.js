ca4d36311e60f7e378fa28d5e024d4f6948bfb8c
pearl <adedijioluwaseun@yahoo.com>
2022-07-05 22:21:35 +0100

    first commit of scoreboard

---
+++ score.js
@@ -0,0 +1,39 @@
+//et scoreEl = document.getElementById(score1).value
+//console.log(scoreEl);
+let score = 0;
+function increase1() {
+  score += 1;
+  console.log("score");
+  document.getElementById("score1").textContent = score;
+}
+
+function increase2(){
+  score += 2;
+  console.log("score");
+  document.getElementById("score1").textContent = score;
+}
+
+function increase3(){
+  score += 3;
+  console.log("score");
+  document.getElementById("score1").textContent = score;
+}
+
+let scoreb = 0
+function increase4() {
+  scoreb += 1;
+  console.log("scoreb");
+  document.getElementById("score2").textContent = scoreb;
+}
+
+function increase5() {
+  scoreb += 2;
+  console.log("scoreb");
+  document.getElementById("score2").textContent = scoreb;
+}
+
+function increase6() {
+  scoreb += 3;
+  console.log("scoreb");
+  document.getElementById("score2").textContent = scoreb;
+}
